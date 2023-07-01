7f8db358779393c39a1709c453776c89b5411ee2
Usuario <usuario@MacBook-Air-de-Usuario.local>
2023-07-01 17:50:45 +0200

    feat: .gitignore fix and more

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "28ca781a493d4a44"; }
+/******/ 		__webpack_require__.h = function() { return "0aae7e171a288f76"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,7 @@
-import { HiHome, HiUser, HiRectangleGroup, HiViewColumns, HiChatBubbleBottomCenterText, HiEnvelope } from 'react-icons/hi';
+import { HiHome, HiUser, HiRectangleGroup, HiViewColumns, HiChatBubbleBottomCenterText, HiEnvelope } from 'react-icons/hi2';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-
-//  links
 const links = [
   { name: 'home', path: '/', icon: <HiHome /> },
   { name: 'about', path: '/about', icon: <HiUser /> },
@@ -22,7 +21,17 @@ const links = [
 
 
 const Nav = () => {
-  return <nav>nav</nav>;
+
+  const router = useRouter();
+  const { pathname } = router;
+  return <nav>
+    <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80
+    xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+      {links.map((link, index) => {
+        return <Link href={link.path}>{link.icon}</Link>
+      })}
+    </div>
+  </nav>;
 };
 
 export default Nav;

cddbdb9169d348a96ec1b9be23d6c11547fed250
Usuario <usuario@MacBook-Air-de-Usuario.local>
2023-07-02 20:36:27 +0200

    feat: Socials done

--- components/Socials.js
+++ components/Socials.js
@@ -4,17 +4,17 @@ import Link from 'next/link';
 import { RiYoutubeLine, RiInstagramLine, RiLinkedinLine, RiGithubLine } from 'react-icons/ri'
 
 const Socials = () => {
-  return <div>
-    <Link href='https://www.youtube.com/channel/UCP0CaJFUN2eqP9oJZ92Xmew' className='hover:text-accent transition-all duration-300'>
+  return <div className='flex items-center gap-x-5 text-lg'>
+    <Link target="_blank" href='https://www.youtube.com/channel/UCP0CaJFUN2eqP9oJZ92Xmew' className='hover:text-accent transition-all duration-300'>
       <RiYoutubeLine />
     </Link>
-    <Link href='https://www.instagram.com/prattchach/' className='hover:text-accent transition-all duration-300'>
+    <Link target="_blank" href='https://www.instagram.com/prattchach/' className='hover:text-accent transition-all duration-300'>
       <RiInstagramLine />
     </Link>
-    <Link href='https://www.linkedin.com/in/prateek-chachra-3a2030a7/' className='hover:text-accent transition-all duration-300'>
+    <Link target="_blank" href='https://www.linkedin.com/in/prateek-chachra-3a2030a7/' className='hover:text-accent transition-all duration-300'>
       <RiLinkedinLine />
     </Link>
-    <Link href='https://github.com/prateekchachra' className='hover:text-accent transition-all duration-300'>
+    <Link target="_blank" href='https://github.com/prateekchachra' className='hover:text-accent transition-all duration-300'>
       <RiGithubLine />
     </Link>
   </div>;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,10 @@
 import { FaFigma, FaHtml5, FaCss3, FaJs, FaReact, FaWordpress } from 'react-icons/fa';
 import { SiAdobexd, SiAdobephotoshop, SiNextdotjs, SiFramer } from 'react-icons/si';
+
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+import { useState } from 'react';
+
 const aboutData = [
   {
     title: 'skills',
@@ -71,8 +76,36 @@ const aboutData = [
   },
 ];
 
+// Framer motion
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  return (<div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+    <Circles />
+    <motion.div variants={fadeIn('right', 0.2)}
+    initial='hidden'
+    animate='show'
+    exit='hidden'
+    className='hidden xl:flex absolute bottom-0 -left-[370px]'>
+      <Avatar />
+    </motion.div>
+    <div className='container mx-auto h-full flex flex-col items-center xl:flex-row
+    gap-x-6'>
+      <div>text</div>
+      <div>
+        <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>{aboutData.map((item,itemIndex) => (
+          <div key={itemIndex} className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px]
+          after:bg-white after:absolute after:-bottom-1 after:left-0`}
+          onClick={() => setIndex(itemIndex)}>
+            {item.title}
+          </div>
+        ))}</div>
+      </div>
+    </div>
+  </div>);
 };
 
 export default About;

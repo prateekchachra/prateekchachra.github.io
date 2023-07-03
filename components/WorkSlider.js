df938928ba787f73344f19721e18644a90772095
Usuario <usuario@MacBook-Air-de-Usuario.local>
2023-07-03 13:19:44 +0200

    feat: Work slide

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,6 +1,6 @@
 
 
-import {RxMobile, RxPencil2, RxDesktop, RxReader, RxRocket, RxArrowTopRight } from 'react-icons/rx';
+import {BsArrowRight } from 'react-icons/bs';
 import {Swiper, SwiperSlide} from 'swiper/react';
 
 import 'swiper/css'
@@ -60,16 +60,26 @@ const WorkSlider = () => {
     clickable: true
   }}
   modules={[Pagination]}
-  className='h-[320px] sm:h-[380px]'
+  className='h-[280px] sm:h-[340px]'
   >
     {workSlides.slides.map((slide,index) => (
       <SwiperSlide key={index}>
-        <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
+        <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer max-h-[100%]'>
           {slide.images.map((image,ind) => 
           (
-            <div className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
-              <div className='flex items-center justify-center relative overflow-hidden'>
+            <div key={ind} className='relative rounded-lg overflow-hidden flex items-center justify-center group '>
+              <div className='flex items-center justify-center relative overflow-hidden group'>
                <Image src={image.path} width={500} height={300} alt={image.title}/>
+               <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#E838CC] 
+               to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
+               <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10
+               transition-all duration-300'>
+                <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
+                  <div className='delay-100'>LIVE</div>
+                  <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
+                  <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight /></div>
+                </div>
+              </div>
               </div>
             </div>
           ))}
